6b4fe6fc4f26abfac38fe4b002a3b80321fa360a
heyZeuss <rubioj@protonmail.com>
2019-02-04 20:20:53 -0500

    Random Test Quiz

---
+++ projects/rubioj/quiz/testme.c
@@ -0,0 +1,78 @@
+// José-Antonio D. Rubio
+// CS 362-400
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    char random_char;
+
+    //testing for characters that are in the range of
+    //32(space) to 126('~')
+    random_char = (rand() % (126 +1 - 32)) + 32;
+
+    return random_char;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    char *test_string = malloc (sizeof (char) * 6);
+    int i;
+
+    //Fill in first 5 elements of array with random characters
+    //between 101 ('e') and 116 ('t')
+    for(i=0; i< 5; i++)
+    {
+      test_string[i] = (rand() % (116 + 1 - 101))+ 101;
+    }
+    test_string[5] = '\0';
+
+   return test_string;
+    
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
